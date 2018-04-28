010a5ec623791acfa40ee1ada20c7727053ab931
ecsteele <steelee3@nku.edu>
2018-04-28 15:06:37 -0400

    Updated to add Mate's file and working accuracy function.

--- gluontest.py
+++ gluontest.py
@@ -35,6 +35,15 @@ class MyaDevDataset(mx.gluon.data.Dataset):
             item = self.transform(item)
         
         return item
+    
+def accuracy(predictions, labels):
+    pred = predictions.asnumpy()[:,0]
+    lab = labels.asnumpy()[:,0]
+    correct = 0
+    for i in range(len(pred)):
+        if abs(pred[i] - lab[i]) < 0.33:
+            correct += 1
+    return correct
 
 def main():
     #ds = mx.gluon.data.vision.MNIST(train=True, transform=lambda data, label: (data, label))
@@ -43,7 +52,7 @@ def main():
         
     # Generate the training set, with nxn images.
     n= 36
-    trset_size= 3000
+    trset_size= 5000
     print('Generating training set...')
     X,Y = nmd.makeDataSet(n,trset_size, training=True)
     # For convolutional neural nets, we want 2d single plane images.
@@ -83,50 +92,77 @@ def main():
     
     # Define the model architecture
     with net.name_scope():
-        net.add(gluon.nn.Conv2D(channels=nf, kernel_size=(5,5), use_bias=True, activation='relu'))
+        net.add(gluon.nn.Conv2D(channels=20, kernel_size=(5,5), use_bias=True, activation='relu'))
+        net.add(gluon.nn.MaxPool2D(pool_size=ps, strides=ps))
+        net.add(gluon.nn.BatchNorm())
+        net.add(gluon.nn.Conv2D(channels=40, kernel_size=(5,5), use_bias=True, activation='relu'))
+        net.add(gluon.nn.MaxPool2D(pool_size=ps, strides=ps))
+        net.add(gluon.nn.Flatten())
+        net.add(gluon.nn.Dense(20, activation="relu", use_bias=True))
+        net.add(gluon.nn.Dropout(0.3))
+        net.add(gluon.nn.Dense(15, activation="relu", use_bias=True))
+        net.add(gluon.nn.Dense(1, activation="sigmoid", use_bias=True)) # Output layer
+        """net.add(gluon.nn.Conv2D(channels=nf, kernel_size=k, use_bias=True, activation='relu'))
         net.add(gluon.nn.MaxPool2D(pool_size=ps, strides=ps))
         net.add(gluon.nn.Flatten())
         net.add(gluon.nn.Dense(nh, activation="relu", use_bias=True))
-        net.add(gluon.nn.Dense(1, activation="relu", use_bias=True)) # Output layer
+        net.add(gluon.nn.Dense(1, activation="sigmoid", use_bias=True)) # Output layer"""
+
     
     # We start with random values for all of the model’s parameters from a 
     # normal distribution with a standard deviation of 0.05
-    #net.collect_params().initialize(mx.init.Normal(sigma=0.05))
-    net.collect_params().initialize(mx.init.Uniform())
+    net.collect_params().initialize(mx.init.Normal(sigma=0.05))
+    #net.collect_params().initialize(mx.init.Uniform())
     mean_squared_error = gluon.loss.L2Loss()
     
     # We opt to use the stochastic gradient descent (sgd) training algorithm 
     # and set the learning rate hyperparameter to .1
-    trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': .1})
+    trainer = gluon.Trainer(net.collect_params(), 'SGD', {'learning_rate': .1})
     
     # Loop through several epochs and watch the model improve
-    epochs = 10
-    for e in range(epochs):
+    max_epochs = 20
+    for e in range(max_epochs):
+        correct = 0
         for i, (data, label) in enumerate(train_data):
             data = data.as_in_context(mx.cpu()).swapaxes(3,1)
             label = label.as_in_context(mx.cpu())
             with autograd.record(): # Start recording the derivatives
                 output = net(data) # the forward iteration
                 loss = mean_squared_error(output, label)
+                correct += accuracy(output, label)
                 loss.backward()
             trainer.step(data.shape[0])
             # Provide stats on the improvement of the model over each epoch
             curr_loss = ndarray.mean(loss).asscalar()
-        print("Epoch {}. Current Loss: {}.".format(e, curr_loss))
+        acc = correct / trset_size
+        print("Epoch {}. Current Accuracy: {}. Current Loss: {}.".format(e, acc, curr_loss))
+        if curr_loss < .005:
+            break
     
-    acc = mx.metric.Accuracy()
     # Run against testing set
+    count = 0
     for i, (data, label) in enumerate(test_data):
         data = data.as_in_context(mx.cpu()).swapaxes(3,1)
         label = label.as_in_context(mx.cpu())
         output = net(data)
-        acc.update(labels=label, preds=output)
+        count += accuracy(output, label)
         if i < 10:
             img = data.swapaxes(3,1)
+            print(img[0].shape)
             render_as_image(img[0])
             print("expected: " + str(label) + "| actual: " + str(output))
-            
-    print("Test accuracy: " + str(acc.get()[1]))
+    acc = count / teset_size
+    print("Test accuracy: {}".format(acc))
     
+def main2():
+    pred = np.array([.8, .5, .7, .4, .0, .1, .3])
+    lab = np.array([1, 1, 1, 1, 0, 0, 0])
+    correct = 0
+    for i in range(len(pred)):
+        print(i)
+        print(abs(pred[i] - lab[i]))
+        if abs(pred[i] - lab[i]) < 0.33:
+            correct += 1
+    print(correct)
 
 main()
